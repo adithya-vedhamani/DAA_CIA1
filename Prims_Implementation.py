708c3758641ebda53262146ad6d5a805c4d7d8e4
Adithya <73640313+adithya-vedhamani@users.noreply.github.com>
2023-01-18 11:12:53 +0530

    Update Prims_Implementation.py

--- Prims_Implementation.py
+++ Prims_Implementation.py
@@ -1,59 +1,32 @@
+//Implementation in Prims's Algorithm
+n = int(input("Enter the number of nodes:"))
+cost = [[float('inf') for j in range(n)] for i in range(n)]
+visited = [0 for i in range(n)]
+mincost = 0
+ne = 1
 
-//Implemenation in Prims Algorithm
-from queue import PriorityQueue
-class Graph:
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        cost[i][j] = int(input())
+        if cost[i][j] == 0:
+            cost[i][j] = float('inf')
 
-    def __init__(self, vertices):
-        self.V = vertices   # Total number of vertices in the graph
-        self.graph = []     # Array of edges
+visited[0] = 1
 
-    # Add edges
-    def add_edge(self, s, d, w):
-        self.graph.append([s, d, w])
+while ne < n:
+    min = float('inf')
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if cost[i][j] < min:
+                if visited[i - 1] != 0:
+                    min = cost[i][j]
+                    a = u = i
+                    b = v = j
+    if visited[u - 1] == 0 or visited[v - 1] == 0:
+        print("Edge", ne, ": (", a, ",", b, ")", "Cost :", min)
+        mincost += min
+        visited[b - 1] = 1
+    ne += 1
+    cost[a][b] = cost[b][a] = float('inf')
 
-    # Print the solution
-    def print_solution(self, dist):
-        print("Vertex Distance from Source")
-        for i in range(self.V):
-            print("{0}\t\t{1}".format(i, dist[i]))
-
-    def prims(self, src):
-    # Create an array to store the minimum distance from the source vertex
-        dist = [float("Inf")] * self.V
-        # Mark the source vertex
-        dist[src] = 0
-        # Create a set to store the vertices included in the MST
-        included = set()
-        # Create a priority queue to store the edges
-        pq = PriorityQueue()
-        # Add the edges of the source vertex to the queue
-        for neighbor, weight in self.graph[src]:
-            pq.put((weight, src, neighbor))
-        # Repeat the process until all vertices are included in the MST
-        while len(included) != self.V:
-            # Get the edge with the smallest weight
-            weight, s, d = pq.get()
-            # If the destination vertex of the edge is already in the MST, skip it
-            if d in included:
-                continue
-            # Add the destination vertex to the MST
-            included.add(d)
-            # Update the minimum distance of the destination vertex
-            dist[d] = weight
-            # Add the edges of the destination vertex to the queue
-            for neighbor, weight in self.graph[d]:
-                if neighbor not in included:
-                    pq.put((weight, d, neighbor))
-    # Print the solution
-            self.print_solution(dist)
-
-
-
-g = Graph(5)
-g.add_edge(0, 1, 5)
-g.add_edge(0, 2, 4)
-g.add_edge(1, 3, 3)
-g.add_edge(2, 1, 6)
-g.add_edge(3, 2, 2)
-
-g.prims(0)
+print("\nMinimum cost=", mincost)
